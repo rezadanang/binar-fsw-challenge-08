a30f8e879addb9b23e24d616b690a995a5103b32
rezadanang <rezadanang23@gmail.com>
2022-11-16 19:45:48 +0700

    first commit

---
+++ app/controllers/ApplicationController.spec.js
@@ -0,0 +1,117 @@
+const { NotFoundError } = require('../errors');
+const ApplicationController = require('./ApplicationController');
+
+describe("ApplicationController", () =>{
+    describe("#handleGetRoot", () => {
+        it("should call res.status(200) and res.json with status and message", () => {
+            
+            const mockRequest = {};
+            const mockResponse = {
+                status: jest.fn().mockReturnThis(),
+                json: jest.fn().mockReturnThis(),
+              };
+
+            const applicationController = new ApplicationController()
+            
+            applicationController.handleGetRoot(mockRequest, mockResponse);
+        
+            expect(mockResponse.status).toHaveBeenCalledWith(200);
+            expect(mockResponse.json).toHaveBeenCalledWith({
+                status: "OK",
+                message: "BCR API is up and running!",
+            });
+
+        })
+    })
+
+    describe("#handleNotFound", () =>{
+        it("should call res.status(404) and res.json with error not found", () => {
+            const mockRequest = {
+                method: 'get',
+                url: 'localhost:8000'
+            };
+
+            const err = new NotFoundError(mockRequest.method, mockRequest.url);
+
+            const mockResponse = {
+                status: jest.fn().mockReturnThis(),
+                json: jest.fn().mockReturnThis(),
+            }; 
+            
+            const applicationController = new ApplicationController()
+
+            applicationController.handleNotFound(mockRequest, mockResponse);
+            expect(mockResponse.status).toHaveBeenCalledWith(404);
+            expect(mockResponse.json).toHaveBeenCalledWith({
+                error: {
+                  name: err.name,
+                  message: err.message,
+                  details: err.details,
+                }
+              });
+        })
+    })
+
+    describe("#handleError", () =>{
+        it("should call res.status(500) and res.json with error", () => {
+            const mockRequest = {
+                method: 'get',
+                url: 'localhost:8000'
+            };
+            
+            const err = new NotFoundError(mockRequest.method, mockRequest.url);
+
+            const mockResponse = {
+                status: jest.fn().mockReturnThis(),
+                json: jest.fn().mockReturnThis(),
+            };
+            
+            const mockNext = {}
+            const applicationController = new ApplicationController()
+
+            applicationController.handleError(err, mockRequest, mockResponse, mockNext);
+            expect(mockResponse.status).toHaveBeenCalledWith(500);
+            expect(mockResponse.json).toHaveBeenCalledWith({
+                error: {
+                  name: err.name,
+                  message: err.message,
+                  details: err.details || null,
+                }
+              }); 
+        })
+    })
+
+    describe("#getOffsetFromRequest", () =>{
+        it("should return offset request", () => {
+            const mockRequest = {
+                query: {
+                   page: 1,
+                   pageSize: 10 
+                }
+            };
+            const applicationController = new ApplicationController()
+            const offset = applicationController.getOffsetFromRequest(mockRequest)
+            expect(offset).toEqual(0)
+        })
+    })
+
+    // describe("#buildPaginationObject", () =>{
+    //     it("should return build pagination", () => {
+    //         const mockRequest = {
+    //             query: {
+    //                 page: 1,
+    //                 pageSize: 10 
+    //              }
+    //         }
+    //         const applicationController = new ApplicationController()
+    //         const pageCount = applicationController.buildPaginationObject(mockRequest)
+    //         expect(pageCount).toBe({
+    //             page,
+    //             pageCount,
+    //             pageSize,
+    //             count,
+    //           });
+    //     })
+    // })
+
+})
